c40afa76e91cd88f193dd5152198dd07d12ffe8e
Eric Gray <eric@ericgray.tech>
2023-08-15 11:58:49 -0400

    Added outputs.

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,13 @@ output "client_certificate" {
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.aks_1.kube_config_raw
-
   sensitive = true
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks_1.name
+}
+
+output "rg_name" {
+  value = azurerm_resource_group.rg.name
 }
